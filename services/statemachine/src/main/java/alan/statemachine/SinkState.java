bf3d7fa1b3b8dc3d0fd3d47cc0fdbed649624ef6
Kumar Ishan <kumarishan@aianash.com>
2017-05-29 00:57:43 +0530

    update: [Refactor] SinkState in separate file

---
+++ services/statemachine/src/main/java/alan/statemachine/SinkState.java
@@ -0,0 +1,75 @@
+package alan.statemachine;
+
+import java.util.function.Function;
+import java.util.function.BiFunction;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.CompletableFuture;
+
+
+/**
+ *
+ */
+class SinkState<S, SMC, R> {
+
+  private final S name;
+  private final Class<R> resultType;
+  private final BiFunction<SMC, ExecutorService, CompletableFuture<R>> asyncAction;
+  private final Function<SMC, R> action;
+  private final boolean isSuccess;
+
+  public SinkState(S name, Class<R> resultType, Function<SMC, R> action, boolean isSuccess) {
+    this.name = name;
+    this.resultType = resultType;
+    this.asyncAction = null;
+    this.action = action;
+    this.isSuccess = isSuccess;
+  }
+
+  public SinkState(S name, Class<R> resultType, BiFunction<SMC, ExecutorService, CompletableFuture<R>> asyncAction, boolean isSuccess) {
+    this.name = name;
+    this.resultType = resultType;
+    this.asyncAction = asyncAction;
+    this.action = null;
+    this.isSuccess = isSuccess;
+  }
+
+  /**
+   *
+   */
+  public Class<?> getResultType() {
+    return resultType;
+  }
+
+  /**
+   * [getAction description]
+   * @return [description]
+   */
+  public Function<SMC, R> getAction() {
+    return action;
+  }
+
+  /**
+   * [getAsyncAction description]
+   * @return [description]
+   */
+  public BiFunction<SMC, ExecutorService, CompletableFuture<R>> getAsyncAction() {
+    return asyncAction;
+  }
+
+  /**
+   * [isSuccess description]
+   * @return [description]
+   */
+  public final boolean isSuccess() {
+    return isSuccess;
+  }
+
+  /**
+   * [isActionAsync description]
+   * @return [description]
+   */
+  public final boolean isActionAsync() {
+    return this.asyncAction != null;
+  }
+
+}
